647d4c21d65bf84e4057c216a47ff7837a0f050e
MenaRMadarth <menakar273@gmail.com>
2022-04-03 20:55:58 +0530

    fisrt commit

---
+++ src/pages/RouterPage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+import Home from './Home'
+import About from './About'
+import Manufacturingcap from './Manufacturingcap'
+import Flavours from './Flavours'
+import Fragrances from './Fragrances'
+import Contact from './Contact'
+import Contacts from './Contacts'
+import Career from './Career'
+//import Mailchimp from './Mailchimp'
+import App from '../App';
+
+
+export default function RouterPage() {
+    return(
+        <div>
+            <Router>
+            <Header />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/manufacturingcap" component={Manufacturingcap} />
+                    <Route path="/flavours" component={Flavours} />
+                    <Route path="/fragrances" component={Fragrances} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/contacts" component={Contacts} />
+                    <Route path="/career" component={Career} />
+                    {/* <Route path="/mailchimp" component={Mailchimp} /> */}
+                </Switch>
+            <Footer />
+            </Router>
+        </div>
+    )
+}
